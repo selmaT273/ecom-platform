bc3c3fdc2044995b94fd613aac1ca33fca0f74b9
Stacey Teltser <stacey.teltser@gmail.com>
2021-09-02 17:06:32 -0500

    created mock data and started tabbed nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation';
+import Main from './components/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <BrowserRouter>
+          <Navigation />
+          <Main />
+        </BrowserRouter>
+      </>
+    );
+  }
 }
-
-export default App;
